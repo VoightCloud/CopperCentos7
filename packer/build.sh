b94e2f08d14b2c54eb9b836f1182338e8d702007
Jeff Voight <jeff@voight.org>
2021-12-18 22:37:31 -0500

    No need to echo token. That part of the debugging is finished.

--- packer/build.sh
+++ packer/build.sh
@@ -3,12 +3,12 @@
 export password=`openssl rand -base64 9`
 
 echo $password
+
 hash=`openssl passwd -6 $password`
 
 sed -i -E "s|\-\-password=(.*)|--password=$hash|g" http/ks-proxmox.cfg
 
 #packer build --force vb_aws.pkr.hcl
-echo ${PROXMOX_TOKEN}
 packer build --force proxmox.pkr.hcl
 
 sed -i -E "s|\-\-password=(.*)|--password=randpass|g" http/ks-proxmox.cfg
